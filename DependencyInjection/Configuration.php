2edea44b81d9183f98f1ef9a41f5690fcbfc7935
Damon Jones <damon@insig.net>
2012-06-25 17:05:00 -0400

    First commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace YDD\VebraApiBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('vebra');
+        $rootNode
+            ->children()
+                ->scalarNode('username')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('password')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('data_feed_id')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('token_directory')->isRequired()->cannotBeEmpty()->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ YDDVebraApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace YDD\VebraApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YDDVebraApiBundle extends Bundle
+{
+}
